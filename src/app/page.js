8d2e2af8160479f6fc223660ff77e3ee40f2a873
Kathleen Tillman <me@saecula.io>
2024-06-06 15:33:46 -0400

    imporove into readability

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import page from "./page2.svg";
+import intro from "./intro.svg";
 
 const text = "Welcome! this is my personal site and it's under construction. for now you can find me at github.com/saecula or say hello at hi@saecula.io."
 
 const Home = () => {
     return (<main className={styles.main}>
-       <Image src={page} alt={text} height={700} />
+       <Image src={intro} alt={text} height={600} />
     </main>)
 }
 
