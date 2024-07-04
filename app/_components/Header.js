c7ed8845538e000690304ae3ebd3da37035c09e5
Kathleen Tillman <admin@saecula.io>
2024-07-04 17:10:54 -0400

    add dummy login icon, move external links

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -5,23 +5,15 @@ import styles from './style.module.css';
 
 const iconSize = 40;
 
-const HeaderIcon = ({ href, src }) => (
-  <Link href={href} target="_blank" rel="noopener noreferrer">
-    <img src={src} width={iconSize} height={iconSize} />
-  </Link>
-);
-
 const Header = () => (
   <header className={styles.header}>
-    <div className={styles.group}>
+    <div id="home" className={styles.group}>
       <Link href="/">
         <img src="/mascot.svg" width={iconSize} height={iconSize} />
       </Link>
     </div>
-    <div className={styles.group}>
-      <HeaderIcon href="https://www.github.com/saecula" src="/gh.svg" />
-      <HeaderIcon href="https://www.linkedin.com/in/saecula/" src="/in.svg" />
-      <HeaderIcon href="https://www.x.com/_saecula" src="/x.svg" />
+    <div id="dummy" className={styles.group}>
+      <img src="/login.svg" width={iconSize} height={iconSize} />
     </div>
   </header>
 );
--- app/page.js
+++ app/page.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import Link from 'next/link';
 
 import styles from './style.module.css';
 
-const altText =
-  "Welcome! this is my personal site and it's under construction. for now you can find me at github.com/saecula or say hello at admin@saecula.io.";
+const iconSize = 40;
+
+const Icon = ({ href, src }) => (
+  <Link href={href} target="_blank" rel="noopener noreferrer">
+    <img src={src} width={iconSize} height={iconSize} />
+  </Link>
+);
 
 export default () => {
   return (
-    <div className={styles.splash}>
+    <div className={styles.page}>
       <div>about</div>
       <div>blog</div>
-      <div>github</div>
-      <div>linkedin</div>
+      <div className={styles.links}>
+        <Icon href="https://www.linkedin.com/in/saecula/" src="/in.svg" />
+        <Icon href="https://www.github.com/saecula" src="/gh.svg" />
+        <Icon href="https://www.x.com/_saecula" src="/x.svg" />
+      </div>
     </div>
   );
 };
