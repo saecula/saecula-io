d8316a6bb3aed939940b00d1a48674d9d7e3f801
Kathleen Tillman <admin@saecula.io>
2024-07-24 17:42:26 -0400

    fix animation

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 
 import styles from './style.module.css';
@@ -9,51 +10,70 @@ const inUrl = 'https://www.linkedin.com/in/saecula';
 const ghUrl = 'https://www.github.com/saecula';
 const xUrl = 'https://www.x.com/_saecula';
 
+const Fade1 = ({ animate }) => (
+  <div
+    className={animate ? styles.fade1 : styles.quickfade}
+  >{`I'm good at picturing systems as their simplest moving parts.`}</div>
+);
+
+const Fade2 = ({ animate }) => (
+  <div
+    className={animate ? styles.fade2 : styles.quickfade}
+  >{`I enjoy using this to build gadgets that solve real-world problems really well.`}</div>
+);
+
+const Fade3 = ({ animate }) => (
+  <div className={animate ? styles.fade3 : styles.quickfade}>
+    <div className={styles.calltoaction}>
+      Feel free to <Link href={mailto}>email me</Link>, check out my{' '}
+      <Link target="_blank" rel="noopener noreferrer" href={resumeUrl}>
+        resume
+      </Link>
+      , or visit one of the links below.
+    </div>
+    <svg
+      width={30}
+      height={30}
+      className={animate ? styles.arrow : styles.quickarrow}
+      viewBox="0 0 100 100"
+    >
+      <path d="M 0,0 L 50,40 L 100,0" />
+    </svg>
+    <div className={styles.links}>
+      <Icon href={inUrl} src="/in.svg" />
+      <Icon href={ghUrl} src="/gh.svg" />
+      <Icon href={xUrl} src="/x.svg" />
+    </div>
+  </div>
+);
+
 const Icon = ({ href, src }) => (
   <Link href={href} target="_blank" rel="noopener noreferrer">
     <img src={src} width={45} height={45} />
   </Link>
 );
 
-const Fade1 = () => (
-  <div
-    className={styles.fade1}
-  >{`I'm good at picturing systems as their simplest moving parts.`}</div>
-);
-
-const Fade2 = () => (
-  <div
-    className={styles.fade2}
-  >{`I enjoy using this to build gadgets that solve real-world problems really well.`}</div>
-);
-
-const Fade3 = () => (
-  <div className={styles.fade3}>
-    <div className={styles.calltoaction}>
-      Feel free to <Link href={mailto}>email me</Link>, check out my{' '}
-      <Link href={resumeUrl}>resume</Link>, or visit one of the links below.
-    </div>
-    <svg width={30} height={30} className={styles.arrow} viewBox="0 0 100 100">
-      <path d="M 0,0 L 50,40 L 100,0" />
-    </svg>
-  </div>
-);
-
 export default () => {
+  const [animate, setAnimate] = useState(true);
+
+  useEffect(() => {
+    const animationSeen = localStorage.getItem('animationSeen');
+    if (animationSeen) {
+      setAnimate(false);
+    } else {
+      localStorage.setItem('animationSeen', true);
+    }
+  }, []);
+
   return (
     <div className={styles.page}>
       <div className={styles.intro}>
         <img src="/intro.svg" alt="Hi, I'm Kathleen." width={300} height={45} />
       </div>
       <div className={styles.fadegroup}>
-        <Fade1 />
-        <Fade2 />
-        <Fade3 />
-      </div>
-      <div className={styles.links}>
-        <Icon href={inUrl} src="/in.svg" />
-        <Icon href={ghUrl} src="/gh.svg" />
-        <Icon href={xUrl} src="/x.svg" />
+        <Fade1 animate={animate} />
+        <Fade2 animate={animate} />
+        <Fade3 animate={animate} />
       </div>
     </div>
   );
